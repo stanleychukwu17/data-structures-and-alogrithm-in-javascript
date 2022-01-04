adc0eeb9ed78281bbb0c3d1b8111c0b34cee6b06
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-04 13:21:54 +0100

    changes

--- 0 - testing/index.js
+++ 0 - testing/index.js
@@ -7,10 +7,49 @@ class Node {
 
 class LinkedList {
     constructor(value) {
-        this.head = new Node
+        this.head = new Node(value)
         this.tail = this.head
+        this.length = 1;
+    }
+
+    append (value) {
+        const newNode = new Node(value)
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++;
+    }
+
+    prepend (value) {
+        const newNode = new Node(value)
+        newNode.next = this.head
+        this.head = newNode
+        this.length++;
+    }
+
+    get_the_node_before (index) {
+
+    }
+
+    insert (value) {
+        const newNode = new Node(value)
+
+    }
+
+    printList() {
+        const items = []
+        let currentNode = this.head
+
+        while (currentNode !== null) {
+            items.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        console.log(items)
+        return items
     }
 }
 
 const myList = new LinkedList(10);
-console.log(myList);
+myList.append(16)
+myList.prepend(1)
+// console.log(myList);
+myList.printList()

6afdf04d1a37ce577845c3b31b6db0d8e52387dc
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-04 13:01:44 +0100

    changes

--- 0 - testing/index.js
+++ 0 - testing/index.js
@@ -1,1 +1,16 @@
-console.log('we doing good')
+class Node {
+    constructor (value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        this.head = new Node
+        this.tail = this.head
+    }
+}
+
+const myList = new LinkedList(10);
+console.log(myList);

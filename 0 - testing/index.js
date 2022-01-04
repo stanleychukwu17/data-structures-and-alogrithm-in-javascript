b33fe507d497d8b5a15bcf171a370919d26ac113
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-04 13:40:30 +0100

    changes

--- 0 - testing/index.js
+++ 0 - testing/index.js
@@ -27,17 +27,18 @@ class LinkedList {
     }
 
     get_the_node_before (index) {
-        let Node;
-        let i = 0;
-        for (let i = 0; i < index.length; i++) {
-            const uy = index[i];
-            
-        }
+        let node = this.head;
+        for (let i = 1; i < index; i++) { node = node.next }
+        return node
     }
 
-    insert (value) {
+    insert (index, value) {
         const newNode = new Node(value)
-
+        const nodeBefore = this.get_the_node_before(index)
+        newNode.next = nodeBefore.next
+        nodeBefore.next = newNode
+        this.length++
+        return this;
     }
 
     printList() {
@@ -55,6 +56,10 @@ class LinkedList {
 
 const myList = new LinkedList(10);
 myList.append(16)
+myList.append(17)
+myList.append(18)
+myList.append(19)
 myList.prepend(1)
+myList.insert(4, 22)
 // console.log(myList);
 myList.printList()

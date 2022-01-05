354befaf4595b860e089314a314930656da0e7ee
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-05 07:47:43 +0100

    changes

--- 0 - testing/windy.js
+++
@@ -1,6 +0,0 @@
-const cursor = {x:0, y:0};
-window.addEventListener('mousemove', event => {
-    cursor.x = (event.clientX / 800) - 0.5
-    cursor.y = (event.clientY / 600) - 0.5
-    console.log(cursor)
-})

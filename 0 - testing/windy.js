98853da34f7db2b7953baacb70d895973ac832e0
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-05 07:32:27 +0100

    changes

--- 0 - testing/windy.js
+++ 0 - testing/windy.js
@@ -1,1 +1,6 @@
-console.log('windey')
+const cursor = {x:0, y:0};
+window.addEventListener('mousemove', event => {
+    cursor.x = (event.clientX / 800) - 0.5
+    cursor.y = (event.clientY / 600) - 0.5
+    console.log(cursor)
+})

1f1685f3cbc2d22c774fae6230bee203786c6d68
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-07 07:32:49 +0100

    changes

--- 0 - testing/index.js
+++ 0 - testing/index.js
@@ -1,3 +1,4 @@
+// the class below creates a new node
 class Node {
     constructor (value) {
         this.value = value;
@@ -5,20 +6,24 @@ class Node {
     }
 }
 
+
 class LinkedList {
+    // the constructor creates the head and the tail of the linked list, also we store the total number of items in the list(i.e the this.length)
     constructor(value) {
         this.head = new Node(value)
         this.tail = this.head
         this.length = 1;
     }
 
+    // attaches a new node to the end of the linked list
     append (value) {
         const newNode = new Node(value)
-        this.tail.next = newNode
+        this.tail.next = newNode // since the tail is the last item, we make the new node to be the new tail
         this.tail = newNode
         this.length++;
     }
 
+    // attaches a new node to the begining of the linked list
     prepend (value) {
         const newNode = new Node(value)
         newNode.next = this.head
@@ -26,12 +31,14 @@ class LinkedList {
         this.length++;
     }
 
+    //gets the node(i.e an item) before a certian index
     get_the_node_before (index) {
-        let node = this.head;
-        for (let i = 1; i < index; i++) { node = node.next }
+        let node = this.head; // this.head is the complete list of nodes
+        for (let i = 1; i < index; i++) { node = node.next } // loops through the nodes until we get to the index we are looking for
         return node
     }
 
+    // attaches a new node at a particular index
     insert (index, value) {
         const newNode = new Node(value)
         const nodeBefore = this.get_the_node_before(index)
@@ -41,10 +48,11 @@ class LinkedList {
         return this;
     }
 
+    // deletes a node from the linked list
     remove (index) {
-        const nodeBefore = this.get_the_node_before(index - 1)
-        const toDeleteNode = nodeBefore.next
-        nodeBefore.next = toDeleteNode.next
+        const nodeBefore = this.get_the_node_before(index - 1) // get the node before the one we want to delete
+        const toDeleteNode = nodeBefore.next // the next one is the one we're deleting
+        nodeBefore.next = toDeleteNode.next // once we skip the current node by jumping it, since there is no other pointer to the object/node, the javascript gabage collector deletes the object and reclaims the space in the memory
         this.length--
         return this
     }

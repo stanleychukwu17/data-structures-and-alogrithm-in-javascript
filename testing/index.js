e1d81a6e25e8eddf17321a064d0efb402c4f84e1
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-08 11:53:04 +0100

    changes

---
+++ testing/index.js
@@ -0,0 +1,22 @@
+class Node {
+    constructor (value) {
+        this.value = value;
+        this.right = null
+        this.left = null
+    }
+}
+
+class BinarySearchTree {
+    constructor () {
+        this.root = null
+    }
+
+    insert (node) {}
+
+    lookup (node) {}
+
+    remove (node) {}
+}
+
+const tree = new BinarySearchTree()
+console.log(tree)

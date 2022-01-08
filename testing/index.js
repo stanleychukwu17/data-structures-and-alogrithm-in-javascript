e7c8ef6750739f41f34365650322594be910922f
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-08 13:10:26 +0100

    changes

--- testing/index.js
+++ testing/index.js
@@ -11,12 +11,70 @@ class BinarySearchTree {
         this.root = null
     }
 
-    insert (node) {}
+    insert (value) {
+        const newNode = new Node(value)
+        let continueTraverse = true
+        let currentNode = null, where_to_attach_node
 
-    lookup (node) {}
+        if (this.root === null) {
+            this.root = newNode
+            return this
+        } else {
+            currentNode = this.root
+        }
 
-    remove (node) {}
+        while (continueTraverse) {
+            if (value > currentNode.value) {
+                if (currentNode.right === null) {
+                    where_to_attach_node = 'right'
+                    continueTraverse = false
+                } else {
+                    currentNode = currentNode.right
+                }
+            } else if (value < currentNode.value) {
+                if (currentNode.left === null) {
+                    where_to_attach_node = 'left'
+                    continueTraverse = false
+                } else {
+                    currentNode = currentNode.left
+                }
+            }
+        }
+
+        // console.log(where_to_attach_node)
+        if (where_to_attach_node === 'right') {
+            currentNode.right = newNode
+        } else if (where_to_attach_node === 'left') {
+            currentNode.left = newNode
+        }
+
+        return this
+    }
+
+    lookup (value) {}
+
+    remove (value) {}
+
+}
+
+
+// the function below is from the teacher to test if our binary tree is working, but am learning some things from his recursion ability here
+function traverse(node) {
+    const tree = {value: node.value}
+
+    tree.left = node.left === null ? null : traverse(node.left)
+    tree.right = node.right === null ? null : traverse(node.right)
+
+    return tree
 }
 
 const tree = new BinarySearchTree()
-console.log(tree)
+tree.insert(9)
+tree.insert(4)
+tree.insert(1)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(6)
+// console.log(tree)
+console.log(traverse(tree.root))

82085748254111c7d1167e465865fad6a2740560
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-11 15:23:20 +0100

    changes

--- testing/index.js
+++ testing/index.js
@@ -18,3 +18,12 @@ class Graph {
 
     }
 }
+
+const myGraph = new Graph()
+myGraph.addVertex(0)
+myGraph.addVertex(1)
+myGraph.addVertex(2)
+myGraph.addVertex(3)
+myGraph.addVertex(4)
+myGraph.addVertex(5)
+myGraph.addVertex(6)

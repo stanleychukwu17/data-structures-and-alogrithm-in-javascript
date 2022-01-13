c911e23723b3851d561e050549f37b46319ca49d
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-13 10:43:49 +0100

    changes

--- testing/index.js
+++ testing/index.js
@@ -7,17 +7,35 @@ class Graph {
 
     // adds a new node to the graph
     addVertex(node) {
-        console.log(node)
+        const nodes = Object.keys(this.adjacentList);
+
+        if (!nodes.includes(node)) {
+            this.adjacentList[node] = []
+            this.numberOfNodes++;
+        }
+        return nodes
     }
 
     // this is an undirected graph
     addEdge(node1, node2) {
+        const nodes = Object.keys(this.adjacentList);
+        if (!nodes.includes(node1)) { return 'node 1 is not a graph node' }
+        if (!nodes.includes(node2)) { return 'node 2 is not a graph node' }
 
+        this.adjacentList[node1].push(node2)
+        this.adjacentList[node2].push(node1)
+        return this.adjacentList[node1]
     }
 
     // displays all of the nodes and it's connections
     showConnections () {
+        const nodes = this.adjacentList
+        let toLog, x
 
+        for (x in nodes) {
+            toLog = `${x} --> ${nodes[x].join(', ')}`
+            console.log(toLog)
+        }
     }
 }
 
@@ -29,3 +47,14 @@ myGraph.addVertex(3)
 myGraph.addVertex(4)
 myGraph.addVertex(5)
 myGraph.addVertex(6)
+
+myGraph.addEdge('3', '1')
+myGraph.addEdge('3', '4')
+myGraph.addEdge('4', '2')
+myGraph.addEdge('4', '5')
+myGraph.addEdge('1', '2')
+myGraph.addEdge('1', '0')
+myGraph.addEdge('0', '2')
+myGraph.addEdge('6', '5')
+// console.log(myGraph)
+myGraph.showConnections()

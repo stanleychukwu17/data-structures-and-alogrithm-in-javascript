4586023925a8bc7a5f3307df045589e1a6fd9092
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-14 09:18:04 +0100

    changes

---
+++ testing/index.js
@@ -0,0 +1,11 @@
+// 0 1 1 2 3 5 8 13 21 34 55 89 144 233 ...
+function fibonacciIterative (n) {
+    if (n < 2) {
+        return n
+    }
+
+    return 'no be 2'
+}
+
+const answer = fibonacciIterative(8)
+console.log(answer)

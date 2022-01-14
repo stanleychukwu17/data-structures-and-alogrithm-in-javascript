74a23cc9be365fc232c9d0bc4d0f58e1111c7d3a
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-14 08:53:03 +0100

    changes

--- testing/index.js
+++ testing/index.js
@@ -3,9 +3,9 @@ function fibonacciRecursive (n) {
 }
 
 /**
- * My iterative approach below is not the best
- * 
- *
+ * My iterative approach below is not the best and like i said in the note, this is the fault of the teacher, the way he explained the problem was the cause
+ * So in my solution, i started from 0 and worked my way upwards until i got to the target number.. the teacher solution is the best and that's what i'll be using if
+ * asked this same question in an interview, but i feel like mine is a bit more solid because my for loop has no base case
 */
 function fibonacciIterative (n) {
     let total = 0

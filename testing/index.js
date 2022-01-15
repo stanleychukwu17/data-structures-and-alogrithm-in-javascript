976af86ca4acdfd88e219140bb7ffb95419ebb5e
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-15 19:48:45 +0100

    changes

--- testing/index.js
+++ testing/index.js
@@ -1,31 +1,55 @@
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+// const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+const numbers = [99, 44, 6, 2, 1, 5, 63, 44, 87, 283, 4, 0]
 
-function selectionSort (array) {
+function inserstionSort (array) {
     let length = array.length
+    let sortedArray = []
+    sortedArray.push(array[0])
 
-    for (let i = 0; i < length; i++) {
-        let smallest = array[i]
-        let finalSmallest = smallest
-        let finalPostion = 0
+    for (let i = 1; i < length; i++) {
+        let firstItem = sortedArray[0]
+        let lastItem = sortedArray[sortedArray.length - 1]
+        let currentItem = array[i]
 
-        for (let j = i; j < length; j++) {
-            /**
-             * the code below could be factorized, that is we remove the finalSmallest varaible and instead use the finalPostion to access the finalSmallest value
-            */
-            if (array[j] < finalSmallest) {
-                finalSmallest = array[j];
-                finalPostion = j
+        console.log('checking for ', currentItem)
+        if (currentItem > lastItem) {
+            console.log('yes sir')
+            sortedArray.push(currentItem)
+        } else if (currentItem < firstItem) {
+            sortedArray = [currentItem, ...sortedArray]
+        } else {
+            let temp = []
+            let index = 0
+            let item
+
+            while (sortedArray[index]) {
+                item = sortedArray[index]
+
+                if (currentItem <= item) {
+                    temp.push(currentItem)
+                    break
+                }
+
+                // if (index > 11) {
+                //     break
+                // }
+
+                temp.push(item)
+                index++
             }
-        }
 
-        // means we have a new minimum, So we swap the items
-        if (finalPostion > 0) {
-            array[i] = finalSmallest
-            array[finalPostion] = smallest
+            // console.log(temp, index, 'the temp \n')
+            // console.log('before slice', sortedArray.join(','))
+            sortedArray = [...temp, ...sortedArray.slice(index)]
+            // console.log(sortedArray.join(','), '\n')
         }
-
-        console.log(smallest, finalSmallest, finalPostion, array.join(','), 'bom \n')
+        console.log('after checking ', sortedArray.join(', '), '\n')
     }
+
+    console.log('done no in', sortedArray.join(', '))
+    console.log('looking for', '0, 1, 2, 4, 5, 6, 44, 44, 63, 87, 99, 283')
+    // console.log(sortedArray.join(', '), array.length, array)
+
 }
 
-selectionSort(numbers)
+inserstionSort(numbers)

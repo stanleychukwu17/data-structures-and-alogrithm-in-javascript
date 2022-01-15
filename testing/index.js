0ae4553f8593d4c30f287ff28f8f0e1325a5a31e
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-15 08:19:55 +0100

    changes

---
+++ testing/index.js
@@ -0,0 +1,28 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+function selectionSort (array) {
+    let length = array.length
+
+    for (let i = 0; i < length; i++) {
+        let smallest = array[i]
+        let finalSmallest = smallest
+        let finalPostion = 0
+
+        for (let j = i; j < length; j++) {
+            if (array[j] < finalSmallest) {
+                finalSmallest = array[j];
+                finalPostion = j
+            }
+        }
+
+        if (finalPostion > 0) {
+            array[i] = finalSmallest
+            array[finalPostion] = smallest
+        }
+
+        console.log(smallest, finalSmallest, finalPostion, array, 'bom \n')
+
+    }
+}
+
+selectionSort(numbers)

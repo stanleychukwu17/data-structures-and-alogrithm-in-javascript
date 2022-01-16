d4f97c2dae2ad769844f71b6fdacff75ff34e2ad
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-16 13:15:28 +0100

    changes

--- testing/index.js
+++ testing/index.js
@@ -3,19 +3,17 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 44, 87, 283, 4, 0]
 
 function inserstionSort (array) {
     let length = array.length
-    let sortedArray = []
+    let sortedArray = [] // here i'll store the sorteditems in the array
     sortedArray.push(array[0])
 
     for (let i = 1; i < length; i++) {
-        let firstItem = sortedArray[0]
-        let lastItem = sortedArray[sortedArray.length - 1]
-        let currentItem = array[i]
+        let firstItem = sortedArray[0] // the lowest item in the array
+        let lastItem = sortedArray[sortedArray.length - 1] // the highest item in the array
+        let currentItem = array[i] // the current item in-check
 
-        console.log('checking for ', currentItem)
-        if (currentItem > lastItem) {
-            console.log('yes sir')
+        if (currentItem > lastItem) { // append 0(1)
             sortedArray.push(currentItem)
-        } else if (currentItem < firstItem) {
+        } else if (currentItem < firstItem) { // prepend using an 0(1) method
             sortedArray = [currentItem, ...sortedArray]
         } else {
             let temp = []

54aa595eba9765d0dfbd2a2e2fd077f8bd703765
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-16 14:27:06 +0100

    changes

--- testing/index.js
+++ testing/index.js
@@ -1,7 +1,9 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 44, 87, 283, 4, 0]
 
 function mergeSort (array) {
-    console.log('we merging now')
+    if (array.length <= 1) return array
+
+    console.log('carry ob')
 }
 
 

4064594f4cd171607ac9e2e2d3b50a212c6fd3c1
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-01-16 13:52:49 +0100

    changes

--- testing/index.js
+++ testing/index.js
@@ -1,10 +1,11 @@
-// const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 const numbers = [99, 44, 6, 2, 1, 5, 63, 44, 87, 283, 4, 0]
+for (let i = 0; i < 1000; i++) { numbers.push(99, 44, 6, 2, 1, 5, 63, 44, 87, 283, 4, 0) }
 
 function inserstionSort (array) {
     let length = array.length
     let sortedArray = [] // here i'll store the sorteditems in the array
     sortedArray.push(array[0])
+    let t0 = performance.now()
 
     for (let i = 1; i < length; i++) {
         let firstItem = sortedArray[0] // the lowest item in the array
@@ -36,9 +37,12 @@ function inserstionSort (array) {
         }
     }
 
-    console.log('done no in', sortedArray.join(', '))
+    // console.log('done no in', sortedArray.join(', '))
+    let t1 = performance.now()
+    console.log(t1 - t0)
     return sortedArray
 }
 
 // call the function
-inserstionSort(numbers)
+const james = inserstionSort(numbers)
+// console.log(james)
